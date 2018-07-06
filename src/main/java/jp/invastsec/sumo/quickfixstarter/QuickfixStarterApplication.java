4b17eeb467b8c079ce5fa0a52dfd610fe74e7c61
Zhang Mingxiang <zack_zhang@invastsec.jp>
2018-07-06 17:40:17 +0900

    init

---
+++ src/main/java/jp/invastsec/sumo/quickfixstarter/QuickfixStarterApplication.java
@@ -0,0 +1,12 @@
+package jp.invastsec.sumo.quickfixstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfixStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfixStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/invastsec/sumo/quickfixstarter/QuickfixStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.invastsec.sumo.quickfixstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuickfixStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
